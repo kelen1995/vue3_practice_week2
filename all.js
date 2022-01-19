0aae17dad9777c39c5102fd7a1597a777f5bb1a7
kelen1995 <kelen@gmail.com>
2022-01-19 17:33:51 +0800

    feat: 完成串接 login API & 儲存 token & 利用 token 進行身分驗證

--- all.js
+++ all.js
@@ -4,9 +4,57 @@ import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.27/vue.e
 const app = createApp({
     data() {
         return {
-            temp: 'hello'
+            apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+            apiPath: 'kn99',
+            email:'',
+            password:''
         }
+    },
+    methods: {
+        login,
+        checkUser,
+    },
+    mounted() {
+        this.checkUser();// 先檢查 token
     }
 });
 
 app.mount('#app');
+
+// 登入
+function login() {
+    axios.post(`${this.apiUrl}/admin/signin`, {
+        'username': this.email,
+        'password': this.password
+    })
+    .then(res => {
+        getToken(res.data);
+        enterProductsPage();
+    })
+    .catch(err => {
+        console.log(err.response);
+        alert(err.response.data.message);
+    })
+}
+
+// 儲存 token 至 cookie
+function getToken(data) {
+    const {token, expired} = data;
+    document.cookie = `hextoken=${token}; expired=${new Date(expired)}; path=/;`;
+}
+
+// 檢查是否有登入 token
+function checkUser() {
+    axios.defaults.headers.common.Authorization = document.cookie.replace(/(?:(?:^|.*;\s*)hextoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    axios.post(`${this.apiUrl}/api/user/check`)
+    .then(res => {
+        enterProductsPage();
+    })
+    .catch(err => {
+        console.log(err.response);
+    })
+}
+
+function enterProductsPage() {
+    window.location = "products.html";
+}
